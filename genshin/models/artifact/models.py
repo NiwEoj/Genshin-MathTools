346bf369cf85af41f595a30c6accd4c0664a6954
NiwEoj <joewin1997@gmail.com>
2021-08-24 15:52:48 +0800

    updated test.py and artifact model.py

--- genshin/models/artifact/models.py
+++ genshin/models/artifact/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Artifact(models.Model):
-    name        = models.TextField(max_length=100)
-    type        = models.IntegerField()
-    passive     = models.TextField(default="none")
-    attack      = models.FloatField(default=0)
-    statTitle   = models.TextField(null=True, default="none")
-    stat        = models.FloatField(default=0)
+    name         = models.TextField(max_length=100)
+    twopcEffect  = models.TextField(default="none")
+    fourpcEffect = models.TextField(default="none")
+    element      = models.TextField(default="none")
+    twopcBonus   = models.TextField(default="none")
+    twopcStats   = models.FloatField(default="none")
+    fourpcBonus  = models.TextField(default="none")
--- test.py
+++ test.py
@@ -3,26 +3,28 @@ from collections import Counter
 chrelem,chrwpn = ('pyro','gs')
 reaction = 'vape'
 ttlatkp = 2.2
+ttlcrit = .5
 ttlem = 0
 ttler = 1
 ttldmgbnsE = [1,1,1,1,1]
 ttldmgbnsP = [1,1,1,1,1]
 ttlreactbnsA = 0
 ttlreactbnsT = 0
-arttype = ['glad','glad','cw','cw','cw']
+arttype = ['wt','wt','wt','wt','glad']
 artstack = 3        # how to add this into user interface?
 artDB = {'Name':['glad','cw','shimenawa','hod','fate','bc','pf','no','wt','scholar','martial'],
          'Rarity':[5,5,5,5,5,5,5,5,5,4,4],
          'twopc':['Atk','Edmg','Atk','Edmg','ER','Pdmg','Pdmg','Burst','EM','ER','Atk'],
          'twopcelem':['n','pyro','n','hydro','n','n','n','n','n','n','n'],
          'twopcStt':[.18,.15,.18,.15,.2,.25,.25,.2,80,.2,.18],
-         'fourpc':[('normal'),('reactbnsA','reactbnsT','Edmg'),('normal','charge','plunge'),('normal','charge'),('burst'),('charge'),('atk','Pdmg'),(''),('charge'),(''),('normal','charge')],
+         'fourpc':[('normal'),('reactbnsA','reactbnsT','Edmg'),('normal','charge','plunge'),('normal','charge'),(''),('charge'),('atk','Pdmg'),(''),('charge'),(''),('normal','charge')],
          'fourpcstacks':[(1),(1,1,3),(1,1,1),(1,1),(1),(1),(2,1),(1),(1),(1),(1,1)],
          'fourpcStt':[(.35),(.15,.4,.075),(.5,.5,.5),(.3,.3),(),(.5),(.09,.25),(),(.35),(),(.25,.25)],
          'fourpcreactT':[(),('ov','burn'),(),(),(),(),(),(),(),(),()]}
 
+# uptime = (period of time when passive is up) / (period of time for a rotation)
 def artDef(artName,pcnum,stack=1,uptime=1):
-    global ttlatkp,ttldmgbnsE,ttldmgbnsP,ttlreactbnsT,ttlreactbnsA,ttlem,ttler
+    global ttlatkp,ttldmgbnsE,ttldmgbnsP,ttlreactbnsT,ttlreactbnsA,ttlem,ttler,ttlcrit
     idx = artDB['Name'].index(artName)
     if artDB['twopc'][idx] == 'Atk':
         ttlatkp = ttlatkp + artDB['twopcStt'][idx]
@@ -39,7 +41,7 @@ def artDef(artName,pcnum,stack=1,uptime=1):
     elif artDB['twopc'] == 'ER':
         ttler = ttler + artDB['twopcStt'][idx]
     if pcnum >= 4:
-        for i in range(len(artDB['fourpc'][idx])):
+        for i in range(len(artDB['fourpc'][idx])):                          # len of length=1 tuple got fetched as string instead of tuple
             stacks = min(stack,artDB['fourpcstacks'][idx][i])
             if artDB['fourpc'][idx][i] == 'normal':
                 ttldmgbnsE[0] = ttldmgbnsE[0] + artDB['fourpcStt'][idx][i] * uptime
@@ -62,14 +64,15 @@ def artDef(artName,pcnum,stack=1,uptime=1):
                 for j in range(len(ttldmgbnsE)):
                     ttldmgbnsE[j] = ttldmgbnsE[j] + artDB['fourpcStt'][idx][i] * stacks * uptime
             elif artDB['fourpc'][idx][i] == 'Pdmg':
-                for j in range(len(ttldmgbnsP)):
-                    ttldmgbnsP[j] = ttldmgbnsP[j] + artDB['fourpcStt'][idx][i] * uptime 
+                if ((artName == 'pf') and (stack == 2)) or (artName != 'pf'):
+                    for j in range(len(ttldmgbnsP)):
+                        ttldmgbnsP[j] = ttldmgbnsP[j] + artDB['fourpcStt'][idx][i] * uptime 
             elif artDB['fourpc'][idx][i] == 'atk':
-                if ((artName == 'pf') and (stacks != 2)) or (artName != 'pf'):
-                    ttlatkp = ttlatkp + artDB['fourpcStt'][idx][i] * uptime
+                ttlatkp = ttlatkp + artDB['fourpcStt'][idx][i] * stacks * uptime
+            elif artDB['fourpc'][idx][i] == 'crit':
+                ttlcrit = ttlcrit + .2 * stacks
         if artName == 'fate':
-            fatebns = min(ttler*.25,.75)
-            ttldmgbnsE[2] = ttldmgbnsE[2] + fatebns                    
+            ttldmgbnsE[2] = ttldmgbnsE[2] + min(ttler * .25,.75)             
 
 artCount = Counter(arttype)
 b = artCount.most_common(2)
@@ -79,7 +82,8 @@ if b[1][1] > 1:
     artDef(b[1][0],b[1][1])
 
 print('ttlatkp: ',ttlatkp)
-print('ttldmgbnsE',ttldmgbnsE)
+print('ttldmgbnsE: ',ttldmgbnsE)
+print('ttldmgbnsP: ',ttldmgbnsP)
 print('ttlreactbnsA: ',ttlreactbnsA)
-print('ttlreactbnsT',ttlreactbnsT)
+print('ttlreactbnsT: ',ttlreactbnsT)
 
