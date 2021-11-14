1bc42c387b5bc55dbff9b8b94a86f3a45d37b54e
looiyaowei <looiyaowei@gmail.com>
2021-11-14 18:26:23 +0800

    add characters

--- genshin/static/admin/js/statsManagement.js
+++ genshin/static/admin/js/statsManagement.js
@@ -1,57 +1,114 @@
-var totalHp = [],
-    totalAttack = [],
-    totalDefence = [];
+var total_hp = [],
+    total_attack = [],
+    total_defence = [],
+    total_em = [],
+    total_critRate = [],
+    total_selfHeal = [],
+    total_recharge = [],
+    total_physDmg = [];
 
+var hpPercent = [],
+    attackPercent = [],
+    defencePercent = [];
 
-function getHp() {
+// index(source): 0 - character, 1 - weapon, 2 - artifacts
+function setPercent(value, stat, index) {
 
-    var total = 0;
+    switch (stat) {
+        case "hp":
+            hpPercent[index] = value;
+            break;
+        case "attack":
+            attackPercent[index] = value;
+            break;
+        case "defence":
+            defencePercent[index] = value;
+            break;
+    };
 
-    for (i = 0; i < totalHp.length; i++) {
-        total += totalHp[i];
-    }
-
-    return total;
 }
 
-function getAttack() {
+function setStats(value, stat, index) {
 
-    var total = 0;
+    var temp;
+    eval("temp = total_" + stat)
 
-    for (i = 0; i < totalAttack.length; i++) {
-        total += totalAttack[i];
+    temp[index] = value;
+
+    let total = 0;
+
+    for (i = 0; i < temp.length; i++) {
+        total += temp[i];
     }
 
-    return total;
-}
-
-function getDefence() {
-
-    var total = 0;
-
-    for (i = 0; i < totalDefence.length; i++) {
-        total += totalDefence[i];
-    }
-
-    return total;
+    $("#character_" + stat).text(total);
 }
 
 function setHp(hp, index) {
 
-    totalHp[index] = hp;
-    $("#character_hp").text(getHp())
+    total_hp[index] = hp;
+
+    let base = 0,
+        totalPercent = 0;
+
+    for (i = 0; i < total_hp.length; i++) {
+        base += total_hp[i];
+    }
+
+    for (i = 0; i < hpPercent.length; i++) {
+        totalPercent += hpPercent[i];
+    }
+
+    let result = Math.floor(base * (1+ totalPercent));
+
+    $("#character_hp").text(result);
 
 }
 
 function setAttack(attack, index) {
 
-    totalAttack[index] = attack;
-    $("#character_attack").text(getAttack())
+    total_attack[index] = attack;
+
+    let base = 0,
+        totalPercent = 0;
+
+    for (i = 0; i < total_attack.length; i++) {
+        base += total_attack[i];
+    }
+
+    for (i = 0; i < attackPercent.length; i++) {
+        totalPercent += attackPercent[i];
+    }
+
+    /** 
+     * base - Base Attack
+     * totalPercent - ascension + weapon bonus + artifacts(%)
+     * flat - feather + artifacts
+     * */
+
+    let result = Math.floor(base * (1+ totalPercent));
+    // return total * (1 + totalPercent) + flat;
+
+    $("#character_attack").text(result);
 
 }
 
 function setDefence(defence, index) {
 
-    totalDefence[index] = defence;
-    $("#character_defence").text(getDefence())
+    total_defence[index] = defence;
+
+    let total = 0,
+        totalPercent = 0;
+
+    for (i = 0; i < total_defence.length; i++) {
+        total += total_defence[i];
+    }
+
+    for (i = 0; i < defencePercent.length; i++) {
+        totalPercent += defencePercent[i];
+    }
+
+    let result = total * (totalPercent + 1);
+
+    $("#character_defence").text(result);
 }
